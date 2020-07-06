eb72663e99389264b33e74bf78e6ac043fd6b86d
enoch chejieh <enochchejieh@gmail.com>
2020-07-06 17:28:31 +0100

    setting change

--- notepad/settings.py
+++ notepad/settings.py
@@ -148,7 +148,7 @@ WEBPACK_LOADER={
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'dist/static'),
+    os.path.join(BASE_DIR, 'dist'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
