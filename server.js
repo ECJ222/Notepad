04441c18912433232f68abaf2890c90003d44c4e
enoch chejieh <enochchejieh@gmail.com>
2020-07-07 15:57:28 +0100

    update

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const serveStatic = require("serve-static")
 const path = require('path');
 
 app = express();
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
+if (process.env.NODE_ENV === 'production'){
+	app.use(express.static('dist')); //all the static files are being served
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'index.html')) //sends response data to index.html
+	});
+}
 
 const port = process.env.PORT || 8080;
 
